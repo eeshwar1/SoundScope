20542b4f440ba3297b0df7e4250739d4eaad363a
Venky Venkatakrishnan <venkyul.official@gmail.com>
2020-04-27 19:56:05 -0500

    Initial Commit

---
+++ SoundScope/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SoundScope
+//
+//  Created by Venky Venkatakrishnan on 4/27/20.
+//  Copyright © 2020 Venky UL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
